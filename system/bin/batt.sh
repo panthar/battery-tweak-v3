13ddb68bf9ca3c691c97ad47cd2515fe8ec79134
Shabbypenguin <shabbypenguin@gmail.com>
2010-09-17 23:29:08 -0700

    merging

--- system/bin/batt.sh
+++ system/bin/batt.sh
@@ -240,3 +240,140 @@ done
 
 
 fi #end here if enabled
+
+
+------------------------------------------------------
+#!/system/bin/sh
+echo "Starting up"
+
+expr="busybox expr"
+#configurable options
+#moved to /system/etc/batt.conf
+
+. /system/etc/batt.conf
+
+#Initialization variables
+#Dont mess with these.
+status="unknown!"
+last_status="unknown";
+batt_life=0;
+current_polling_interval=5;
+current_max_clock=0
+bias=0;
+last_bias=0;
+last_capacity=0;
+#End of init variables
+echo "Defining Functions"
+increase_battery()
+{
+log "collin_ph: Increasing Battery"
+mount -t rfs -o remount,rw /dev/block/stl9 /
+current_polling_interval=$polling_interval_on_battery;
+echo 0 > /proc/sys/vm/swappiness
+echo 0 > /proc/sys/vm/dirty_expire_centisecs
+echo 0 > /proc/sys/vm/dirty_writeback_centisecs
+echo 60 > /proc/sys/vm/dirty_background_ratio
+echo 95 > /proc/sys/vm/dirty_ratio
+echo 10 > /proc/sys/vm/vfs_cache_pressure
+echo $max_freq_on_battery > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo $min_freq_on_battery > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+#echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
+#echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/powersave_bias
+last_capacity=0;
+current_max_clock=$max_freq_on_battery
+mount -t rfs -o remount,ro /dev/block/stl9 /system
+log "collin_ph: Done Increasing Battery"
+}
+
+increase_performance()
+{
+log "collin_ph: Increasing Performance"
+mount -t rfs -o remount,rw /dev/block/stl9 /
+current_polling_interval=$polling_interval_on_power;
+echo 30 > /proc/sys/vm/swappiness
+echo 3000 > /proc/sys/vm/dirty_expire_centisecs
+echo 500 > /proc/sys/vm/dirty_writeback_centisecs
+echo 10 > /proc/sys/vm/dirty_background_ratio
+echo 40 > /proc/sys/vm/dirty_ratio
+echo 10 > /proc/sys/vm/vfs_cache_pressure
+echo $max_freq_on_power > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo $min_freq_on_power > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+#echo 50 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
+#echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/powersave_bias
+last_capacity=0;
+current_max_clock=$max_clock_on_power
+mount -t rfs -o remount,ro /dev/block/stl9 /
+log "collin_ph: Done Increasing Performance"
+}
+set_powersave_bias()
+{
+    capacity=`expr $capacity '*' 10`
+    bias=`expr 1000 "-" $capacity`
+    bias=`expr $bias "/" $battery_divisor`
+    bias=`echo $bias | awk '{printf("%d\n",$0+=$0<0?-0.5:0.5)}'`
+    if $expr "$bias" != "$last_bias" > /dev/nul 
+       then
+       log "collin_ph: Setting powersave bias to $bias"
+       mount -t rfs -o remount,rw /dev/block/stl9 /
+       #echo $bias > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/powersave_bias
+       mount -t rfs -o remount,ro /dev/block/stl9 /
+       last_bias=$bias;
+      log "collin_ph: Done Setting powersave bias"
+       
+    fi
+
+}
+
+set_max_clock()
+{
+    temp=`expr 100 "-" $capacity`
+    temp=`expr $temp "/" $battery_divisor`
+    temp=`expr $max_freq_on_battery "*" $temp`
+    temp=`expr $temp "/" 100`
+    temp=`expr $max_freq_on_battery "-" $temp`
+    temp=`echo $temp | awk '{printf("%d\n",$0+=$0<0?-0.5:0.5)}'`
+
+    if $expr "$temp" != "$current_max_clock" > /dev/nul 
+       then
+       current_max_clock=$temp
+       log "collin_ph: Setting Max Clock to $temp";
+       echo $temp > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+       log "collin_ph: Done Setting Max Clock";
+    fi
+
+
+}
+
+while $expr 1 > /dev/nul 
+do
+status=$(cat /sys/class/power_supply/battery/status);
+capacity=$(cat /sys/class/power_supply/battery/capacity);
+
+sleep $current_polling_interval
+if $expr "$status" != "$last_status" > /dev/nul 
+  then
+     last_status=$status;
+       case $status in
+          "Not charging") increase_battery;;
+          *) increase_performance;;
+       esac
+
+
+fi
+
+
+if $expr "$status" = "Not charging" > /dev/nul
+  then
+  if $expr "$capacity" != "$last_capacity"  > /dev/nul
+    then
+    last_capacity=$capacity
+    log "collin_ph: status = $status"
+    case $cpu_limiting_method in
+       "1") set_max_clock;;
+       "2") set_powersave_bias;;
+    esac
+
+  fi
+fi
+
+done
